deadccf0710e97c86f110b0f806dd72bc24980cb
Carolina A O <carol.amoroli@gmail.com>
2022-06-05 11:31:52 -0300

    project created

---
+++ src/test/java/com/carol/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carol.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
